b46c3314ef4ed56110aac9b6e5a5781d80ee36c3
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-06-19 16:48:20 +0200

    add javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,11 @@
-let revealId = document.querySelector(".reveal");
+let revealId = document.querySelector(".fillimage");
 const button = document.getElementsByTagName("a");
+console.log(revealId);
 
 window.addEventListener('load', function () {
-  setTimeout(1000)
-})
+  setTimeout(() => {
+    revealId.classList.add('is-loaded');
+  }, 3000);
+});
 
-// button.addEventListener('click', () => {
-//   function loadImg() {
-//     let profileImg = document.querySelector('.fillimage');
-//     profileImg.src = image_url;
-//     profileImg.addEventListener('load', () => {
-//       profileImg.style.backgroundImage = 'url(' + image_url + ')';
-//       profileImg.image.className = profileImg.image.className + 'is-loaded';
-//     });
-//   }
-//   return loadImg
-// });
-
-
-// // This is "probably" IE9 compatible but will need some fallbacks for IE8
-// // - (event listeners, forEach loop)
-
-// // wait for the entire page to finish loading
-// window.addEventListener('load', function () {
-
-//   // setTimeout to simulate the delay from a real page load
-//   setTimeout(lazyLoad, 1000);
-
-// });
-
-// function lazyLoad() {
-//   var card_images = document.querySelectorAll('.card-image');
-
-//   // loop over each card image
-//   card_images.forEach(function (card_image) {
-//     var image_url = card_image.getAttribute('data-image-full');
-//     var content_image = card_image.querySelector('img');
-
-//     // change the src of the content image to load the new high res photo
-//     content_image.src = image_url;
-
-//     // listen for load event when the new photo is finished loading
-//     content_image.addEventListener('load', function () {
-//       // swap out the visible background image with the new fully downloaded photo
-//       card_image.style.backgroundImage = 'url(' + image_url + ')';
-//       // add a class to remove the blur filter to smoothly transition the image change
-//       card_image.className = card_image.className + ' is-loaded';
-//     });
-
-//   });
-
-// }
+console.log(revealId);
