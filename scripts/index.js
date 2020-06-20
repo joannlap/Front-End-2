0475623f63a913ab90da77a55b61741a7f84bfeb
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-06-20 15:17:58 +0200

    index.js adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 let revealId = document.querySelector(".fillimage");
 
+console.log(revealId.classList);
 window.addEventListener('load', () => {
   setTimeout(() => {
     revealId.classList.add('is-loaded');
@@ -7,3 +8,13 @@ window.addEventListener('load', () => {
 });
 
 console.log(revealId);
+
+/* MDN contributors, April 2020. Window: load event. Retrieved on 17 June 2020
+https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event */
+
+/* Alligator. Adding, Removing & Toggling Classes With classList in JavaScript. 
+Retrieved on 18 June 2020
+https://alligator.io/js/classlist/ */
+
+/* MDN contributors, August 2019. Blur(). Retrieved on 18 June 2020
+ https://developer.mozilla.org/en-US/docs/Web/CSS/filter-function/blur */
