752b99046dbfce0d740d1f466311800bfcad8770
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-06-29 00:40:04 +0200

    Added scripts/index.js.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const locationContainer = document.querySelector(".location-container");
 const allLocations = Array.from(document.querySelectorAll("h3"));
 const allCards = Array.from(document.querySelectorAll('.filterValue'));
-console.log(allCards)
 
 const locationList = ['Toronto',
   'Vancouver',
@@ -35,11 +34,12 @@ const checkLocation = (target) => {
   allLocations.forEach((cardLocation) => {
     if (cardLocation.innerHTML !== target.innerText) {
       allCards[index].classList.add('remove');
+      console.log(allCards[index])
+      console.log(index)
     }
     index++;
   });
 };
-
 // adds a click function and removes the class and sents getEventTarget
 // to the checklocation function
 locationContainer.onclick = (event) => {
@@ -49,7 +49,6 @@ locationContainer.onclick = (event) => {
   // 
   let target = getEventTarget(event);
   checkLocation(target);
-  console.log(target)
 };
 
 
@@ -57,9 +56,10 @@ locationContainer.onclick = (event) => {
  RESOURCES
  ***********/
 
-// https: //developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types. Retrieved on 19 June 2020
-// https: //dmitripavlutin.com/foreach-iterate-array-javascript/. Retrieved on 24 June 2020
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types. Retrieved on 19 June 2020
+// https://dmitripavlutin.com/foreach-iterate-array-javascript/. Retrieved on 24 June 2020
 // https://www.w3schools.com/howto/howto_js_filter_elements.asp. Retrieved on 24 June 2020
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach. Retrieved on 26 June 2020
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild. Retrieved on 26 June 2020
-// https: //developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from. Retrieved on 27 June 2020
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from. Retrieved on 27 June 2020
+// https://codeburst.io/javascript-increment-and-decrement-8c223858d5ed. Retrieved on 27 June 2020
